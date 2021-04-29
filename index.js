f2bfa54c14e4419ab19c13d7c00e39fe9579d32a
Alex Yevtushenko <a@qx.zone>
2021-04-29 14:20:09 +0300

    Minor cleanup

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
 const TG = require('telegram-bot-api');
 
-const api = new TG({
-  token: process.env.TG_API_TOKEN
-});
-
-// Define your message provider
+const api = new TG({ token: process.env.TG_API_TOKEN });
 const mp = new TG.GetUpdateMessageProvider();
 
-// Set message provider and start API
 api.setMessageProvider(mp);
+
 api.start()
   .then(() => {
     console.log('API is started');
   })
   .catch(console.err);
 
-// Receive messages via event callback
 api.on('update', update => {
-
-  // update object is defined at
-  // https://core.telegram.org/bots/api#update
-  console.log(update);
-
   const chat_id = update.message.chat.id;
 
-  // Send text message
   api.sendMessage({
     chat_id: chat_id,
     text: 'I got following message from you via Webhook: *' + update.message.text + '*',
